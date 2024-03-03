129e3af3107b94de2ee2130569cede0c12032180
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-03-03 13:40:42 +0000

    alpinejs

---
+++ _sitee/alpine.js
@@ -0,0 +1,13 @@
+import Alpine from '/_includes/alpinejs/dist/module.esm.js';
+
+Alpine.data('NameGenerator', () => ({
+    names: ['Ricky', 'Dicky', 'Dawn', 'Bobby', 'Carlos', 'Siarto', 'Rochelle', 'David', 'Jaden', 'Kennedy'],
+    randomName: '',
+
+    generateName() {
+        const randomName = Math.floor(Math.random() * this.names.length);
+        this.randomName = this.names[randomName];
+    }
+}));
+
+Alpine.start();
