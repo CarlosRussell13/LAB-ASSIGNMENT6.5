60e16b9c09618ab60d1257959d0a60c93f21dcac
CarlosRussell13 <133693000+CarlosRussell13@users.noreply.github.com>
2024-03-03 12:48:16 +0000

    alpinejs fix

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
-  eleventyConfig.addPassthroughCopy("alpine.js");
   eleventyConfig.addPassthroughCopy("node_modules");
-
+  eleventyConfig.addPassthroughCopy("alpine.js");
 };
